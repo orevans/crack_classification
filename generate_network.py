80db7fc508ace486ccf47e06b2573e79cf46e908
Owen Evans <ore2000@columbia.edu>
2019-04-01 15:14:28 -0400

    Additions to crack generation code

--- generate_network.py
+++ generate_network.py
@@ -1,45 +1,138 @@
 import numpy as np
 from scipy.ndimage.morphology import distance_transform_edt
+from scipy.misc import imsave
+import sys
 
-def initiation_probabilities(grid, gamma):
-    """Return probability matrix based on distances to nearest fracture"""
-    # Compute distance matrix
-    d = distance_transform_edt(1 - grid)
+class Network():
+    def __init__(self, gamma, omega, L=256, n=50):
+        self.gamma = gamma
+        self.omega = omega
+        self.L = L
+        self.n = n
+        self.grid = self.create_grid()
 
-    # Raise to the power of gamma
-    d = np.power(d, gamma)
+    def initiation_probabilities(self):
+        """Return probability matrix based on distances to nearest fracture"""
+        # Compute distance matrix
+        d = distance_transform_edt(1 - self.grid)
 
-    # Normalize by Z = \sum_ij d_ij
-    d /= np.sum(d)
+        # Raise to the power of gamma
+        d[d != 0] = np.power(d[d != 0], self.gamma)
 
-    return d
+        # Normalize by Z = \sum_ij d_ij
+        d /= np.sum(d)
 
-def create_grid(L):
-    """Create grid of size L by L (with 1's on the boundaries and 0's o/w)"""
-    # Set up grid (L by L pixels)
-    grid = np.zeros((256,256))
+        return d
 
-    # Set boundaries of grid to 1
-    grid[:,0] = 1.
-    grid[0,:] = 1.
-    grid[L - 1,:] = 1.
-    grid[:,L - 1] = 1.
+    def create_grid(self):
+        """Create grid of size L by L (with 1's on the boundaries and 0's o/w)"""
+        # Set up grid (L by L pixels)
+        grid = np.zeros((self.L,self.L))
 
-    return grid
+        # Set boundaries of grid to 1
+        grid[:,0] = 1.
+        grid[0,:] = 1.
+        grid[self.L - 1,:] = 1.
+        grid[:,self.L - 1] = 1.
+
+        return grid
+
+    def propagation_direction(self):
+        """Generate a random direction for fracture to propagate in. For simplicity
+        we only will only allow horizontal, vertical or diagonal directions."""
+        # Propagation directions
+        directions = [[1.,0.],[0.,1.],[1.,1.]]
+
+        # Return a random direction
+        k = np.random.randint(3)
+
+        return directions[k]
+
+    def propagate_fractures(self, i, j):
+        """Propagate fractures according to values of omega:
+        if omega = 0, propagate it opposite directions until both ends terminate
+        if omega = 1, propagate until on of the ends terminates"""
+        # Propagate fracture in random direction
+        v = self.propagation_direction()
+
+        prop = np.random.choice((0,1), p=(self.omega,1. - self.omega))
+
+        i_, j_ = i, j
+        if prop:
+            while True:
+                # Propagate first end until it meets a fracture/boundary
+                i_ += int(v[0])
+                j_ += int(v[1])
+
+                if self.grid[i_,j_] == 1:
+                    break
+                else:
+                    self.grid[i_,j_] = 1
+
+            i_, j_ = i, j
+            while True:
+                # Propagate opposite end until it meets a fracture/boundary
+                i_ -= int(v[0])
+                j_ -= int(v[1])
+
+                if self.grid[i_,j_] == 1:
+                    break
+                else:
+                    self.grid[i_,j_] = 1
+
+        else:
+            i_0, j_0 = i, j
+            i_1, j_1 = i, j
+            while True:
+                # Propagate ends simultaneously until one meets a fracture/boundary
+                i_0 += int(v[0])
+                j_0 += int(v[1])
+                i_1 -= int(v[0])
+                j_1 -= int(v[1])
+
+                if self.grid[i_0,j_0] == 1:
+                    break
+                elif self.grid[i_1,j_1] == 1:
+                    break
+                else:
+                    self.grid[i_0,j_0] = 1
+                    self.grid[i_1,j_1] = 1
+
+    def generate_network(self):
+        """Generate fracture grid and save as a .png image"""
+        for k in range(self.n):
+            # Calculate matrix of initialization probabilities
+            d = self.initiation_probabilities().flatten()
+
+            # Pick location to initiate next fracture
+            i, j = np.unravel_index(np.random.choice(np.arange(self.L**2),p=d),
+                                                    (self.L,self.L))
+
+            # Update grid
+            self.grid[i,j] = 1
+
+            # Propagate fractures
+            self.propagate_fractures(i,j)
+
+        # Save image
+        imsave('images/gamma_{}_omega_{}.png'.format(self.gamma,self.omega),
+             1. - self.grid)
 
 def main():
-    # Grid dimension
-    L = 256
+    # Number of sample
+    s = int(sys.argv[1])
 
-    # Number of fractures to generate
-    n = 50
+    # Pick gamma from a standard normal
+    mu_gamma, sigma_gamma = 0, 1.
+    gamma = np.random.normal(mu_gamma, sigma_gamma, s)
 
-    # Create grid
-    grid = create_grid(L)
+    # Pick omega uniformly
+    omega = np.random.uniform(0., 1., s)
 
-    # Calculation matrix of initialization probabilities
-    gamma = 0.1
-    d = initiation_probabilities(grid, gamma)
+    for k in range(s):
+        print(k)
+        cracks = Network(gamma[k],omega[k])
+        cracks.generate_network()
 
 if __name__=="__main__":
     main()
